ee5e8d9d7b97c91bdb392f95e2034076cc38e960
monoxgas <monoxgas@gmail.com>
2024-02-10 16:49:48 -0700

    Move tags -> Models Update tests Move __xml_tag__ to descriptor Lock litellm version for now Update version

--- tests/test_xml_parsing.py
+++ tests/test_xml_parsing.py
@@ -2,49 +2,8 @@ import typing as t
 from contextlib import nullcontext as does_not_raise
 
 import pytest
-from pydantic import field_validator
 
-from rigging.model import CoreModel
-
-
-class Answer(CoreModel):
-    content: str
-
-
-class Question(CoreModel):
-    content: str
-
-
-class QuestionAnswer(CoreModel):
-    question: Question
-    answer: Answer
-
-
-class YesNoAnswer(CoreModel):
-    boolean: bool
-
-    @field_validator("boolean", mode="before")
-    def parse_str_to_bool(cls, v: t.Any) -> t.Any:
-        if isinstance(v, str):
-            if v.strip().lower().startswith("yes"):
-                return True
-            elif v.strip().lower().startswith("no"):
-                return False
-        return v
-
-
-class CommaDelimitedAnswer(CoreModel, tag="delimited-answer"):
-    content: str
-
-    @property
-    def items(self) -> list[str]:
-        return [i.strip() for i in self.content.split(",")]
-
-    @field_validator("content", mode="before")
-    def parse_str_to_list(cls, v: t.Any) -> t.Any:
-        if not isinstance(v, str) or "," not in v:
-            raise ValueError(f"Cannot parse content as a comma delimited list: {v}")
-        return v
+from rigging.model import Answer, CommaDelimitedAnswer, DelimitedAnswer, Model, Question, QuestionAnswer, YesNoAnswer
 
 
 @pytest.mark.parametrize(
@@ -90,15 +49,29 @@ class CommaDelimitedAnswer(CoreModel, tag="delimited-answer"):
             [(QuestionAnswer, QuestionAnswer(question=Question(content="hello"), answer=Answer(content="world")))],
             id="question_answer",
         ),
+        pytest.param(
+            "<delimited-answer>\n- hello\n - world</delimited-answer>",
+            [(DelimitedAnswer, ["hello", "world"])],
+        ),
+        pytest.param(
+            "<delimited-answer>hello, world, foo | bar</delimited-answer>",
+            [(DelimitedAnswer, ["hello", "world", "foo | bar"])],
+        ),
+        pytest.param(
+            "<delimited-answer>hello / world / foo / bar, test | value</delimited-answer>",
+            [(DelimitedAnswer, ["hello", "world", "foo", "bar, test | value"])],
+        ),
     ],
 )
-def test_xml_parsing(content: str, expectations: list[tuple[CoreModel, str]]) -> None:
+def test_xml_parsing(content: str, expectations: list[tuple[Model, str]]) -> None:
     for model, expected in expectations:
         obj, _ = model.extract_xml(content)  # type: ignore [var-annotated]
         assert obj is not None, "Failed to parse model"
         if isinstance(expected, str):
             assert getattr(obj, "content", None) == expected, "Failed to parse content"
-        elif isinstance(expected, CoreModel):
+        if isinstance(expected, list):
+            assert getattr(obj, "items", None) == expected, "Failed to parse items"
+        elif isinstance(expected, Model):
             assert obj.model_dump() == expected.model_dump(), "Failed to parse model"
 
 
@@ -111,13 +84,18 @@ def test_xml_parsing(content: str, expectations: list[tuple[CoreModel, str]]) ->
         ("<yes-no-answer>No, extra stuff</yes-no-answer>", YesNoAnswer, does_not_raise()),
         ("<yes-no-answer>No, stuff <internal-tag></yes-no-answer>", YesNoAnswer, does_not_raise()),
         ("<yes-no-answer>Invalid</yes-no-answer>", YesNoAnswer, pytest.raises(ValueError)),
-        ("<delimited-answer>hello,world</delimited-answer>", CommaDelimitedAnswer, does_not_raise()),
-        ("<delimited-answer>hello, world</delimited-answer>", CommaDelimitedAnswer, does_not_raise()),
-        ("<delimited-answer>hello, world, </delimited-answer>", CommaDelimitedAnswer, does_not_raise()),
-        ("<delimited-answer>hello</delimited-answer>", CommaDelimitedAnswer, pytest.raises(ValueError)),
-        ("<delimited-answer>hello;test;stuff</delimited-answer>", CommaDelimitedAnswer, pytest.raises(ValueError)),
+        ("<delimited-answer>hello world</delimited-answer>", DelimitedAnswer, pytest.raises(ValueError)),
+        ("<comma-delimited-answer>hello,world</comma-delimited-answer>", CommaDelimitedAnswer, does_not_raise()),
+        ("<comma-delimited-answer>hello, world</comma-delimited-answer>", CommaDelimitedAnswer, does_not_raise()),
+        ("<comma-delimited-answer>hello, world, </comma-delimited-answer>", CommaDelimitedAnswer, does_not_raise()),
+        ("<comma-delimited-answer>hello</comma-delimited-answer>", CommaDelimitedAnswer, pytest.raises(ValueError)),
+        (
+            "<comma-delimited-answer>hello;test;stuff</comma-delimited-answer>",
+            CommaDelimitedAnswer,
+            pytest.raises(ValueError),
+        ),
     ],
 )
-def test_xml_parsing_with_validation(content: str, model: CoreModel, expectation: t.ContextManager[t.Any]) -> None:
+def test_xml_parsing_with_validation(content: str, model: Model, expectation: t.ContextManager[t.Any]) -> None:
     with expectation:
         obj, _ = model.extract_xml(content)  # type: ignore [var-annotated]
