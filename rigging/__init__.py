87cce771cf93a06ad36217d80a352e34b2dd7908
monoxgas <monoxgas@gmail.com>
2024-06-04 15:13:49 -0600

    Version to 1.3.0

--- rigging/__init__.py
+++ rigging/__init__.py
@@ -15,7 +15,7 @@ from rigging.message import Message, MessageDict, Messages
 from rigging.model import Model, attr, element, make_primitive, wrapped
 from rigging.tool import Tool
 
-__version__ = "1.1.1"
+__version__ = "1.3.0"
 
 __all__ = [
     "get_generator",
