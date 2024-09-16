13bb1823040b5efedec79ddf9a4d07c3cf907a9a
monoxgas <monoxgas@gmail.com>
2024-09-16 16:57:44 -0600

    Version to 2.0.5

--- rigging/__init__.py
+++ rigging/__init__.py
@@ -18,7 +18,7 @@ from rigging.prompt import Ctx, Prompt, prompt
 from rigging.tool import Tool
 from rigging.util import await_
 
-__version__ = "2.0.4"
+__version__ = "2.0.5"
 
 __all__ = [
     "get_generator",
