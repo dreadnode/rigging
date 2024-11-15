b026fff84304a4405384a10799d90b479aae2734
monoxgas <monoxgas@gmail.com>
2024-11-15 10:45:29 -0700

    Dependency updates. Bump version

--- rigging/__init__.py
+++ rigging/__init__.py
@@ -18,7 +18,7 @@ from rigging.prompt import Ctx, Prompt, prompt
 from rigging.tool import Tool
 from rigging.util import await_
 
-__version__ = "2.0.5"
+__version__ = "2.0.6"
 
 __all__ = [
     "get_generator",
