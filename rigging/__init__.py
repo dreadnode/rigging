afeb3d816b63b39c780c785cf395114ad1b08d5c
monoxgas <monoxgas@gmail.com>
2025-03-16 18:59:39 -0600

    Version to 2.2.8

--- rigging/__init__.py
+++ rigging/__init__.py
@@ -19,7 +19,7 @@ from rigging.tool import ApiTool, Tool
 from rigging.util import await_
 
 # TODO: Migrate to importlib for this
-__version__ = "2.2.7"
+__version__ = "2.2.8"
 
 __all__ = [
     "get_generator",
