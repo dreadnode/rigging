4f7b8b591586043ccd93f5cd7005071b744e7d32
moo <will@dreadnode.io>
2025-06-15 17:42:19 -0600

    clean up arguments. Test with tokenize.ipynb/gsm8k

--- rigging/data.py
+++ rigging/data.py
@@ -294,7 +294,7 @@ async def chats_to_elastic(
 
 
 async def chats_to_tokens(
-    chat: Chat | t.Sequence[Chat],
+    chat: Chat | None,
     tokenizer: AutoTokenizer,
     *,
     apply_chat_template_kwargs: dict[str, t.Any] | None = None,
